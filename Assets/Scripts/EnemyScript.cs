0c3baf30f22d57d78895b8e680df8c1e532c2dbd
Jonathan Dervin <jdervin@ltu.edu>
2021-02-10 14:17:46 -0500

    Eyes and Movement
    Added eyes on enemies and added enemy script.

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public NavMeshAgent pathfinding;
+    public GameObject muzzle;
+    public GameObject projectile;
+    public GameObject eyes;
+    public float visionRange;
+    public bool pursuing = false;
+    private GameObject target;
+
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(!pursuing)
+        {
+                Ray ray = new Ray(eyes.transform.position, eyes.transform.forward * visionRange);
+
+                Debug.DrawRay(ray.origin, ray.direction*visionRange, Color.red);
+
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit))
+                {
+
+                     Debug.Log("I see somethinng");
+                     pursuing = true;
+                     target = hit.transform.gameObject;
+
+                }
+                
+        }
+        else
+        {
+            if(target==null)
+            {
+                pursuing = false;
+            }
+            pathfinding.SetDestination(target.transform.position);
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/ExplodingScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingScript : MonoBehaviour
+{
+    public Transform explosionEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    
+    
+        if (other.gameObject.tag=="Damage")
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+
+
+        }
+
+
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
